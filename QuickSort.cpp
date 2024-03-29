37ad05f33316763780349db8808a6f7026ec6f90
mauraaninditakirana <kiranamaura351@gmail.com>
2024-03-29 17:41:38 +0700

    menambahkan ; pada step 13, karena tadi terlupa sehingga ada 1 error

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -80,5 +80,5 @@ void q_short(int low, int high)
     q_short(low, j - 1); //step 12
 
     //recursive call to sort the right sub array
-    q_short(j + 1, high) //step 13
+    q_short(j + 1, high); //step 13
 }
