7f05fde5bc29f127e94608e1eab53a0802276602
mauraaninditakirana <kiranamaura351@gmail.com>
2024-03-29 17:01:49 +0700

    perulangan for untuk memasukkan n kedalam array

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -18,4 +18,13 @@ void input() {
         else 
             cout << "\n Maksimum panjang array adalah 20" << endl;
     }
+    cout << "\n--------------------" << endl;
+    cout << "\n Enter Array Element" << endl;
+    cout << "\n--------------------" << endl;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "<" << (i = 1) << ">";
+        cin >> arr[i];
+    }
 }
