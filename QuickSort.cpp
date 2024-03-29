53486b1395a0afdaec03523aa9a5ebb5db29e05a
mauraaninditakirana <kiranamaura351@gmail.com>
2024-03-29 17:26:50 +0700

    penggunaan prosedur swap untuk menukar element dalam index i dengan element pada index j

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -63,5 +63,11 @@ void q_short(int low, int high)
             j--; //step 8
             cmp_count++;
         }
+        cmp_count++;
+        if (i < j) //step 9
+        {
+            //swap the element at index i with the element at index j
+            swap (i,j);
+        }
     }
 }
